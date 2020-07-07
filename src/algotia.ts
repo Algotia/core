f55b542d63527813e5b8dae7a13f21ce990de4ab
Jake Adler <jamtheproducer@gmail.com>
2020-07-07 16:52:29 -0700

    exported types for backfills commands

--- src/algotia.ts
+++ src/algotia.ts
@@ -1,5 +1,19 @@
 import boot from "./lib/boot";
 import { backfill } from "./lib/commands/index";
-import { BootOptions, BackfillOptions, ExchangeConfigOptions } from "./types/index";
+import {
+	BootOptions,
+	BackfillOptions,
+	ExchangeConfigOptions,
+	ListOptions,
+	DeleteOptions
+} from "./types/index";
 
-export { boot, backfill, BootOptions, BackfillOptions, ExchangeConfigOptions };
+export {
+	boot,
+	backfill,
+	BootOptions,
+	BackfillOptions,
+	ExchangeConfigOptions,
+	ListOptions,
+	DeleteOptions
+};
