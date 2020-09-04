e926578e0ca89ad85b39bd9461ceab2e04bb6607
Jake Adler <jamtheproducer@gmail.com>
2020-09-04 16:33:20 -0700

    fix(workflow): trying to fix workflow authentication issue

--- release.config.js
+++ release.config.js
@@ -9,6 +9,15 @@ module.exports = {
 				message:
 					"chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
 			}
+		],
+		[
+			"@semantic-release/github",
+			{
+				assets: [
+					{ path: "dist/asset.min.css", label: "CSS distribution" },
+					{ path: "dist/asset.min.js", label: "JS distribution" }
+				]
+			}
 		]
 	]
 };
